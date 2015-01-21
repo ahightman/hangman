c46cb3b8f17d9e6274db5ff090c901f720db0492
Ashley Hightman <ahightman@gmail.com>
2015-01-21 13:31:51 -0500

    adding all files to new repository

---
+++ js/conditionals.js
@@ -0,0 +1,10 @@
+var name = "Chris";
+var age = 34;
+var favoriteFood;
+
+if (age > 30) {
+     alert(“You need to get a physical.”);
+}
+else {
+     alert(“You are fine.”);
+}
---
+++ js/fortune.js
@@ -0,0 +1,17 @@
+// TODO: Your job is to write logic that will
+// give the user one of three random fortunes
+//
+// Use the alert("Some text value") function
+// to tell the user their fortune.
+
+var luck = (Math.random ( ))
+
+if (luck <= .2) {
+  alert("You will have an awesome day!");
+}
+else if (luck <= .5) {
+  alert("You will have an OKAY day!");
+}
+else {
+  alert("Your day is going to suck!");
+}
